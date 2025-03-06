92f1626a7663bd936207387a599d0fde0df1a309
chamath-shanaka <chamathshanaka19@gmail.com>
2025-03-06 14:59:50 +0530

    refined get_admin_by_email with schedular

--- routes/admin.py
+++ routes/admin.py
@@ -23,39 +23,28 @@ async def create_admin(admin: AdminModel, db_manager: DatabaseManager = Depends(
 
 # get an admin by email
 @router.get("/admins/{email}", response_model=AdminModel, status_code=status.HTTP_200_OK)
-async def get_admin_by_email(email: str, dependencies: dict = Depends(get_db_and_scheduler), task_id: str = None):
+async def get_admin_by_email(email: str, dependencies: dict = Depends(get_db_and_scheduler)):
     db_manager = dependencies["db_manager"]
     scheduler_instance = dependencies["scheduler"]
     route_path = dependencies["route_path"]
-
-    async def get_admin_by_email_task(p_email: str, p_db_manager: DatabaseManager, task_id: str):
-        from controllers.admin import get_admin_by_email_controller
-        try:
-            result = await get_admin_by_email_controller(p_email, p_db_manager)
-            return result  # return the actual result from the controller
-        except HTTPException as e:
-            raise e # reraise to be handled by FastAPI
-        except Exception as e:
-            print(f"Task {task_id} failed: {e}")
-            raise HTTPException(status_code = 500, detail = f"Internal Server Error: {e}")
 
     # create a compatible future for the API
     future = asyncio.Future()
 
     # add the task to the scheduler, pass API's future
     await scheduler_instance.add_task(
-        get_admin_by_email_task, email, db_manager,
+        get_admin_by_email_controller, email, db_manager,
         priority = 2,
         route_path = route_path,
-        task_id = task_id,
+        task_id = None,
         existing_future = future
     )
 
-    # await the future
+    # await for the future
     try:
         result = await future
         print(f"result after future: {result}")
-        return result # return the actual result
+        return result # return the result of controller
     except HTTPException as e:
         raise e #re raise HTTPExceptions
     except Exception as e:
