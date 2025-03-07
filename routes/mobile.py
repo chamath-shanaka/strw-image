ea737415a2f5fed85095e730bf6e876eab922a74
chamath-shanaka <chamathshanaka19@gmail.com>
2025-03-07 20:24:22 +0530

    refactored mobile to work with run_with_scheduler

--- routes/mobile.py
+++ routes/mobile.py
@@ -9,11 +9,12 @@ from controllers.mobile import (
     update_rover_nickname_controller,
     get_flower_count_in_range_controller
 )
-from database import DatabaseManager
-from db_manager import get_db_manager
+from dependencies import get_db_and_scheduler
 from models.schemas import FlowerCountSummary
 from models.userSchemas import UserModel
 from datetime import datetime
+
+from scheduler.runner import run_with_scheduler
 
 router = APIRouter()
 
@@ -21,26 +22,26 @@ router = APIRouter()
 
 # Create user route
 @router.post("/users/", response_model=UserModel, status_code=status.HTTP_201_CREATED)
-async def create_user(user: UserModel, db_manager: DatabaseManager = Depends(get_db_manager)):
-    return await create_user_controller(user, db_manager)
+async def create_user(user: UserModel, dependencies: dict = Depends(get_db_and_scheduler)):
+    return await run_with_scheduler(create_user_controller, user, dependencies=dependencies)
 
 
 # Get user by email route
 @router.get("/users/email/{email}", response_model=UserModel)
-async def get_user_by_email(email: str, db_manager: DatabaseManager = Depends(get_db_manager)):
-    return await get_user_by_email_controller(email, db_manager)
+async def get_user_by_email(email: str, dependencies: dict = Depends(get_db_and_scheduler)):
+    return await run_with_scheduler(get_user_by_email_controller, email, dependencies=dependencies)
 
 
 # Get user by userId route
 @router.get("/users/user-id/{userId}", response_model=UserModel)
-async def get_user_by_user_id(userId: int, db_manager: DatabaseManager = Depends(get_db_manager)):
-    return await get_user_by_user_id_controller(userId, db_manager)
+async def get_user_by_user_id(userId: int, dependencies: dict = Depends(get_db_and_scheduler)):
+    return await run_with_scheduler(get_user_by_user_id_controller, userId, dependencies=dependencies)
 
 
 # Get user by username route
 @router.get("/users/username/{username}", response_model=UserModel)
-async def get_user_by_username(username: str, db_manager: DatabaseManager = Depends(get_db_manager)):
-    return await get_user_by_username_controller(username, db_manager)
+async def get_user_by_username(username: str, dependencies: dict = Depends(get_db_and_scheduler)):
+    return await run_with_scheduler(get_user_by_username_controller, username, dependencies=dependencies)
 
 
 # Update roverIds route
@@ -51,8 +52,8 @@ async def get_user_by_username(username: str, db_manager: DatabaseManager = Depe
 
 # register rover
 @router.put("/users/{userId}/register-rover", response_model=UserModel)
-async def register_rover(userId: int, db_manager: DatabaseManager = Depends(get_db_manager)):
-    return await register_rover_controller(userId, db_manager)
+async def register_rover(userId: int, dependencies: dict = Depends(get_db_and_scheduler)):
+    return await run_with_scheduler(register_rover_controller, userId, dependencies=dependencies)
 
 
 # update rover nickname
@@ -61,9 +62,12 @@ async def update_rover_nickname(
         userId: int,
         roverId: int,
         nickname: str,
-        db_manager: DatabaseManager = Depends(get_db_manager),
+        dependencies: dict = Depends(get_db_and_scheduler),
 ):
-    return await update_rover_nickname_controller(userId, roverId, nickname, db_manager)
+    return await run_with_scheduler(
+        update_rover_nickname_controller, userId, roverId, nickname,
+        dependencies=dependencies
+    )
 
 
 # get all pollination count of a user's rovers' in a given time rage
@@ -72,6 +76,9 @@ async def get_flower_count_in_range(
         userId: int,
         start_date: datetime,
         end_date: datetime,
-        db_manager=Depends(get_db_manager)
+        dependencies: dict = Depends(get_db_and_scheduler)
 ):
-    return await get_flower_count_in_range_controller(userId, start_date, end_date, db_manager)
+    return await run_with_scheduler(
+        get_flower_count_in_range_controller, userId, start_date, end_date,
+        dependencies = dependencies
+    )
